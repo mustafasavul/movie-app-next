d94c34f9fb51dbdb4b185307eb55970358d500aa
Mustafa Savul <mustafasavul4434@hotmail.com>
2021-11-14 02:22:20 +0300

    add data fetch and bind

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,40 @@
 import Layout from "../layout/index";
+import Link from "next/link"
+import unfetch from "isomorphic-unfetch";
+import slug from "slug";
 
-function Homepage() {
+function Homepage({movies}) {
     return <Layout headTitle={"Anasayfa"}>
         <h1>Hello Welcome</h1>
+
+        <ul>
+            {movies.results.map((movie) => (
+                <li key={movie.id}>
+                    <Link href="/movies/[slug]"
+                          as={`/movies/${slug(movie.title)}-${movie.id}`}>
+                        <a>{movie.title}</a>
+                    </Link>
+                </li>
+
+            ))}
+        </ul>
     </Layout>
+}
+
+export async function getStaticProps() {
+    const baseURL = 'https://api.themoviedb.org/3/';
+    const apiKey = 'api_key=6b9aa903068d8a1efb9f84d8ca640133';
+    const apiURL = baseURL + 'movie/popular?' + apiKey;
+
+    // data fetch
+    const data = await unfetch(apiURL)
+    const movies = await data.json();
+
+    return {
+        props: {
+            movies
+        },
+    }
 }
 
 export default Homepage;
--- pages/movies/[slug].js
+++ pages/movies/[slug].js
@@ -1,9 +1,49 @@
 import Layout from "../../layout";
+import unfetch from "isomorphic-unfetch";
+import slug from "slug";
+
+const baseURL = 'https://api.themoviedb.org/3/';
+const apiKey = 'api_key=6b9aa903068d8a1efb9f84d8ca640133';
+const apiURL = baseURL + '{movie_id}?' + apiKey;
 
 function MovieDetail({movie}) {
     return <Layout>
+        <div>
+            {movie.title}
+        </div>
 
+        <figure>
+            <img src={movie.poster_path} alt={movie.title}/>
+        </figure>
     </Layout>
+}
+
+export async function getStaticPaths() {
+    const data = await unfetch(apiURL)
+    const movies = await data.json();
+
+    const paths = movies.results.map(movie => {
+        return {params: {slug: `${slug(movie.title)}-${movie.id}`}}
+    });
+
+    return {
+        paths,
+        fallback: false
+    };
+}
+
+export async function getStaticProps({params}) {
+    // data fetch
+    const id = params.slug.split("-").slice(-1)[0];
+
+    const data = await unfetch(apiURL + id)
+    const movie = await data.json();
+
+    return {
+        props: {
+            movie
+        },
+    }
 }
 
 export default MovieDetail;
